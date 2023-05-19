965409881b34dcf1e195243c355261ee478373da
Josias Silva <josias.asjr@yahoo.com.br>
2023-05-18 23:12:41 -0300

    Update Josias Silva 18/05 23:12

--- Client/Default Rev Rec Dates.js
+++ Client/Default Rev Rec Dates.js
@@ -1,23 +1,3 @@
-/* -------------------------------------------------------------------------------------------------------
-*	
-Function:	Sets the RevRec Start Date to the transaction date when item is selected.
-
-Functionality:
-Sets the RevRec Start Date to the transaction date when item is selected.
-
-Deployment:
-The script can be deployed on Sales form .
-
-Event triggering the script:
-It is called on the field change event item field .( Field Change function )
-
-Custom Fields:
-+------------------------------------------------------------------------------------------------------+
-| S. No.|		Id			|	  Type			|	Level	|		Description							
-+------------------------------------------------------------------------------------------------------+
-No custom fields Required																					
-*-----------------------------------------------------------------------------------------------------------
-*/
 
 // Global Variables
 {
